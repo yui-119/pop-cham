a7482982dffdfcf1d65d0dee46fe38cdde909b13
yui-119 <poppop0206@icloud.com>
2025-02-15 17:29:58 +0900

    home画面完成

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Kiwi_Maru } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const kiwimaru = Kiwi_Maru({
+    weight: "400",
+    subsets: ["latin"],  // 日本語に対応するサブセットを指定
+  });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ja">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=M+PLUS+1p&family=M+PLUS+2:wght@100..900&family=Mochiy+Pop+One&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={kiwimaru.className}>{children}</body>
     </html>
   );
 }
